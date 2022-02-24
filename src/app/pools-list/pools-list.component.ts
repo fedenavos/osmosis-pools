98019faf634bb1be59db49014af0a1eb0fc12803
Federico Navós <fedenavos@gmail.com>
2022-02-24 12:45:34 -0300

    loading animation added

--- src/app/pools-list/pools-list.component.ts
+++ src/app/pools-list/pools-list.component.ts
@@ -8,7 +8,7 @@ import { Pool } from '../models/Pool';
   styleUrls: ['./pools-list.component.css']
 })
 
-export class PoolsListComponent implements OnInit{
+export class PoolsListComponent implements OnInit {
 
   url: string = "https://api-osmosis.imperator.co";
 
@@ -16,17 +16,19 @@ export class PoolsListComponent implements OnInit{
 
   sorted: boolean = false;
 
+  loading: boolean = true;
 
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-      
+
     this.getPools();
 
   }
 
 
-  getPools(): void{
+  getPools(): void {
 
     let route = "/pools/v2/all?low_liquidity=true";
 
@@ -39,7 +41,7 @@ export class PoolsListComponent implements OnInit{
             let pool = new Pool(
               i, resp[i][0].symbol, resp[i][1].symbol, resp[i][0].liquidity
             )
-            if(pool.liquidity >= 1000000) {
+            if (pool.liquidity >= 1000000) {
               this.pools.push(pool);
             }
 
@@ -47,12 +49,12 @@ export class PoolsListComponent implements OnInit{
           this.pools.sort((a, b) => b.liquidity - a.liquidity)
 
           this.getAPR();
-      }
-    )
+        }
+      )
 
   }
 
-  getAPR(): void{
+  getAPR(): void {
 
     let route = "/apr/v2/all"
 
@@ -64,23 +66,25 @@ export class PoolsListComponent implements OnInit{
             let i = this.pools.findIndex(
               (pool: Pool) => pool.id == p.pool_id
             )
-            if(i < 0) return;
+            if (i < 0) return;
             let aprList = p.apr_list;
             let iApr = aprList.findIndex(
-              (x:any) => x.symbol == 'OSMO'
+              (x: any) => x.symbol == 'OSMO'
             )
             let remove = false;
-            if(iApr == -1) {
+            if (iApr == -1) {
               remove = true
               iApr = 0;
             };
             this.pools[i].apr14 = aprList[iApr].apr_14d;
             this.pools[i].apr7 = aprList[iApr].apr_7d;
             this.pools[i].apr1 = aprList[iApr].apr_1d;
-            if(remove) {
+            if (remove) {
               this.pools.splice(i, 1);
               remove = false;
             }
+
+            this.loading = false;
 
           });
         }
@@ -89,9 +93,9 @@ export class PoolsListComponent implements OnInit{
 
   sortTable(i: number): void {
 
-    if(!this.sorted) {
+    if (!this.sorted) {
 
-      switch(i){
+      switch (i) {
         case 14: {
           this.pools.sort((a: any, b: any) => {
             return (b.apr14 - a.apr14);
@@ -123,7 +127,7 @@ export class PoolsListComponent implements OnInit{
 
     } else {
 
-      switch(i){
+      switch (i) {
         case 14: {
           this.pools.sort((a: any, b: any) => {
             return (a.apr14 - b.apr14);
@@ -163,10 +167,10 @@ export class PoolsListComponent implements OnInit{
 
     symbol = symbol.toLowerCase();
 
-    if(symbol == 'atom') symbol = 'cosmos';
-    if(symbol == 'osmo') symbol = 'osmosis';
-    if(symbol == 'xki') symbol = 'ki';
-    if(symbol == 'like') symbol = 'likecoin';
+    if (symbol == 'atom') symbol = 'cosmos';
+    if (symbol == 'osmo') symbol = 'osmosis';
+    if (symbol == 'xki') symbol = 'ki';
+    if (symbol == 'like') symbol = 'likecoin';
 
     urlImg += symbol + '.svg';
 
@@ -178,7 +182,7 @@ export class PoolsListComponent implements OnInit{
 
     let newUrl = event.srcElement.src.slice(0, -3) + 'png';
     event.srcElement.src = newUrl;
-    
+
   }
 
 }
